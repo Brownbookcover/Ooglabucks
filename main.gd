904ff937c960da15f9296c9a7767a02402086c45
brownbookcover <mike.ridgeway123@gmail.com>
2026-08-08 16:40:17 -0400

    Some basic menu messing around

---
+++ main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var background = $Background
+
+var movement_delta = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Up"):
+		if movement_delta < 0:
+			movement_delta = 0
+		movement_delta += 1
+	if Input.is_action_just_pressed("Down"):
+		if movement_delta > 0:
+			movement_delta = 0
+		movement_delta -= 1
+	background.position.y += movement_delta
