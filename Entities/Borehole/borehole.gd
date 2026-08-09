a349f91f0e20513f26b0c833d3c2bd37694d0284
Dion Tryban <dion@tryban.dev>
2026-08-09 01:36:06 -0400

    feat: add borehole

---
+++ Entities/Borehole/borehole.gd
@@ -0,0 +1,55 @@
+class_name Borehole
+extends Node2D
+
+@export var layer_colors: Array[Color] = []
+@export var layer_heights: Array[int] = []
+@export var speed_px: float = 5.0
+@export var hole_width_px: int = 64
+@export var bottom_offset: int = 32
+
+@export var _background_sprite: Sprite2D
+@onready var _background_image: Image
+@onready var _background_texture: ImageTexture
+
+const WIDTH = 256
+const HEIGHT = 144
+
+var depth_px: float = 0.0
+
+func _ready() -> void:
+	assert(len(layer_colors) > 0)
+	assert(len(layer_colors) == len(layer_heights))
+	
+	_background_image = Image.create(WIDTH, HEIGHT, false, Image.FORMAT_RGBA8)
+	_background_texture = ImageTexture.create_from_image(_background_image)
+	_background_sprite.texture = _background_texture
+	
+	_draw_to_background()
+
+
+func _physics_process(delta: float) -> void:
+	depth_px += speed_px * delta
+
+
+func _process(delta: float) -> void:
+	_draw_to_background()
+
+
+func _draw_to_background() -> void:
+	for y in HEIGHT:
+		var color = layer_colors[1]
+		for layer_idx in len(layer_heights):
+			if depth_px - HEIGHT + y <= layer_heights[layer_idx]:
+				color = layer_colors[layer_idx]
+				break
+		
+		var dark_color = Color(color)
+		dark_color.v *= 0.6
+		
+		for x in WIDTH:
+			if y < HEIGHT - bottom_offset and !(x < WIDTH / 2 - hole_width_px / 2 or x > WIDTH / 2 + hole_width_px / 2):
+				_background_image.set_pixel(x, y, dark_color)
+			else:
+				_background_image.set_pixel(x, y, color)
+	
+	_background_texture.update(_background_image)
--- main.gd
+++ main.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@onready var background = $Background
-
 var movement_delta = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -9,13 +7,13 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("Up"):
-		if movement_delta < 0:
-			movement_delta = 0
-		movement_delta += 1
-	if Input.is_action_just_pressed("Down"):
-		if movement_delta > 0:
-			movement_delta = 0
-		movement_delta -= 1
-	background.position.y += movement_delta
+#func _physics_process(delta: float) -> void:
+	#if Input.is_action_just_pressed("Up"):
+		#if movement_delta < 0:
+			#movement_delta = 0
+		#movement_delta += 1
+	#if Input.is_action_just_pressed("Down"):
+		#if movement_delta > 0:
+			#movement_delta = 0
+		#movement_delta -= 1
+	#background.position.y += movement_delta
